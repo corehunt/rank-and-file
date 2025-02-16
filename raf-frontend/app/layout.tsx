165340452e9fd27151e14c8332fdb3ea1e38eaa4
coreyroach <coreyhroach@gmail.com>
2025-02-16 15:55:43 -0500

    prod installed vercel speed insights

--- raf-frontend/app/layout.tsx
+++ raf-frontend/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
           <main className="flex-1">
             {children}
             <Analytics/>
+            <SpeedInsights/>
           </main>
           <Footer />
         </ThemeProvider>
